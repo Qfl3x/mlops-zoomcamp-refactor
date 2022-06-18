1e622be63806a323f2ab148baca1f4e5754d1615
Qfl3x <qfl3x@protonmail.com>
2022-06-18 10:27:42 +0000

    Modularized XGBoost MLFlow logging

--- ModularModel/XGBoost_Search.py
+++ ModularModel/XGBoost_Search.py
@@ -1,3 +1,7 @@
+COMPLEX = True
+MLFLOW_LOG = False
+
+
 import pandas as pd
 
 import pickle
@@ -11,17 +15,17 @@ from hyperopt.pyll import scope
 
 import xgboost as xgb
 
-#Simple preprocessing
-from peprocessors.preprocess_simple_green import preprocess, categorical, numerical, target
+if COMPLEX:
+    #Complex preprocessing
+    from preprocessors.preprocess_complex_green import preprocess, categorical, numerical, target
+else:
+    #Simple prprocessing
+    from peprocessors.preprocess_simple_green import preprocess, categorical, numerical, target
 
-#Complex preprocessing
-from preprocessors.preprocess_complex_green import preprocess, categorical, numerical, target
-
-import mlflow
-
-
-mlflow.set_tracking_uri("sqlite:////home/qfl3x/mlflow/mlflow.db")
-mlflow.set_experiment("nyc-taxi-experiment")
+if MLFLOW_LOG:
+    import mlflow
+    mlflow.set_tracking_uri("sqlite:////home/qfl3x/mlflow/mlflow.db")
+    mlflow.set_experiment("nyc-taxi-experiment")
 
 def vectorize(train_path, val_path): #Vectorizer
     
@@ -48,38 +52,37 @@ X_train, X_val, y_train, y_val, dv = vectorize(train_path, val_path)
 
 train = xgb.DMatrix(X_train, label=y_train)
 valid = xgb.DMatrix(X_val, label=y_val)
-
-def objective_mlflow(params):
-    with mlflow.start_run():
-        mlflow.set_tag("model", "xgboost")
+if MLFLOW_LOG:
+    def objective(params):
+        with mlflow.start_run():
+            mlflow.set_tag("model", "xgboost")
         mlflow.set_tag("preprocessor", "complex")
         mlflow.log_params(params)
         booster = xgb.train(
-            params=params,
-            dtrain=train,
-            num_boost_round=1000,
-            evals=[(valid, 'validation')],
-            early_stopping_rounds=50
-        )
+                params=params,
+                dtrain=train,
+                num_boost_round=1000,
+                evals=[(valid, 'validation')],
+                early_stopping_rounds=50
+                )
         y_pred = booster.predict(valid)
         rmse = mean_squared_error(y_val, y_pred, squared=False)
         mlflow.log_metric("rmse", rmse)
 
-    return {'loss': rmse, 'status': STATUS_OK}
+        return {'loss': rmse, 'status': STATUS_OK}
+else:
+    def objective(params):
+        booster = xgb.train(
+                params=params,
+                dtrain=train,
+                num_boost_round=1000,
+                evals=[(valid, 'validation')],
+                early_stopping_rounds=50
+                )
+        y_pred = booster.predict(valid)
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
 
-def objective(params):
-    booster = xgb.train(
-        params=params,
-        dtrain=train,
-        num_boost_round=1000,
-        evals=[(valid, 'validation')],
-        early_stopping_rounds=50
-    )
-    y_pred = booster.predict(valid)
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
-
-    return {'loss': rmse, 'status': STATUS_OK}
-
+        return {'loss': rmse, 'status': STATUS_OK}
 
 search_space = {
     'max_depth': scope.int(hp.quniform('max_depth', 4, 100, 1)),
@@ -92,7 +95,7 @@ search_space = {
 }
 
 best_result = fmin(
-    fn=objective_mlflow,
+    fn=objective,
     space=search_space,
     algo=tpe.suggest,
     max_evals=50,
